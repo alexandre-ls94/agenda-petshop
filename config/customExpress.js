c3ba1175141f9573a2ccc64540e600081f27f025
Alexandre Lima <xandiie.ls@gmail.com>
2021-01-21 16:53:37 -0300

    Adiciona comentarios

--- config/customExpress.js
+++ config/customExpress.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser');
 module.exports = () => {
     const app = express();
 
+    // BodyParser está convertendo os dados enviados em urlencoded e json para serem utilizados no app
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
+    // Consign está carregando automaticamente os controllers quando inicia a aplicação
     consign()
         .include('controllers')
         .into(app);
--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -1,20 +1,25 @@
 const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
+    // os verbos HTTP recebem como parametros um caminho da url e uma função que tem como parametros req (requisição) e res(resposta)
     app.get('/atendimentos', (req, res) => {
         Atendimento.lista()
+            // then é uma promise que devolve um resultado quando o metodo acima é executado com sucesso
             .then(resultados => res.status(200).json(resultados))
+            // catch é uma promisse que devolve os erros caso o metodo acima tenha falha
             .catch(erros => res.status(400).json(erros))
     });
 
     // :id - parametro que sera enviado atraves da url da requisição
     app.get('/atendimentos/:id', (req, res) => {
+        // req.params é onde ficam os parametros enviados na url da requisição
         const id = parseInt(req.params.id);
 
         Atendimento.buscarPorId(id, res);
     });
 
     app.post('/atendimentos', (req, res) => {
+        // req.body é onde ficam os dados enviados no corpo da requisição 
         const dadosAtendimento = req.body;
 
         Atendimento.adiciona(dadosAtendimento)
